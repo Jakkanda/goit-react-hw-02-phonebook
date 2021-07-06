25633c98ac030aff6b1a891a6e4d89e30a1406e5
Jakkanda <51709348+Jakkanda@users.noreply.github.com>
2021-07-06 14:55:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,11 +8,15 @@ class App extends Component {
     name: '',
   };
 
+  handlerSubmit = e => {
+    e.preventDefault();
+  };
+
   render() {
     return (
       <div className="App">
         <h2>Phonebook</h2>
-        <Form />
+        <Form onSubmit={this.handlerSubmit} />
         <h2>Contacts</h2>
         <ul>
           <li></li>
